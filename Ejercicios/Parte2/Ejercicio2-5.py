b58f7e12636db9bd563e3c3aabf7e890016f67bb
Pablo Crespo Cervantes <63744631+doggymux@users.noreply.github.com>
2022-11-15 21:59:07 +0100

    Add files via upload

--- Ejercicios/Parte2/Ejercicio2-5.py
+++ Ejercicios/Parte2/Ejercicio2-5.py
@@ -1,21 +1,20 @@
 #Escribir una función sum() y una función multip() que sumen y multipliquen
 #respectivamente todos los números de una lista. Por ejemplo: sum([1,2,3,4]) debería
 #devolver 10 y multip([1,2,3,4]) debería devolver 24.
-import numpy 
+
 
 lista = []
 n = int(input("Cuantos numeros quieres multipiclicar mi rey: "))
-  
+suma = 0
+multi = 1
 # bucleciño
+
 for i in range(0, n):
     numerico = int(input())
-    suma =+ numerico
-    multi =* numerico
+    suma += numerico
+    multi *= numerico
     
     lista.append(numerico)
-      
-print(numpy.prod(lista))
-print(sum(lista))
 
 print(suma)
 print(multi)
